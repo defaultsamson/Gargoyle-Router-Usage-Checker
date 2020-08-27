e93cb92a0c3d9e7b2069eceb54846343f0e88863
Dankrushen <henryclose@gmail.com>
2020-08-27 18:59:42 -0400

    Add Gargoyle profile data & add method to parser

--- gargoyleparser.h
+++ gargoyleparser.h
@@ -4,14 +4,13 @@
 #include "gargoyleprofile.h"
 #include <QList>
 
-
-
 class GargoyleParser
 {
 public:
     GargoyleParser();
 
-    void update(QList<GargoyleProfile>);
+    /// Fetches the HTML of the Gargoyle page and parses the information into the profiles, appending new profiles as needed
+    void update(QString url, QList<GargoyleProfile> profiles);
 };
 
 #endif // GARGOYLEPARSER_H

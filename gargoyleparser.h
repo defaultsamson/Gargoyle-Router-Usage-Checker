e9f13e13d66d8091ec1f41b3c2888a58ae562a58
Dankrushen <henryclose@gmail.com>
2020-08-27 21:15:42 -0400

    Add network error handling

--- gargoyleparser.h
+++ gargoyleparser.h
@@ -7,10 +7,12 @@
 class GargoyleParser
 {
 public:
+    unsigned int timeout = 5000;
+
     GargoyleParser();
 
     /// Fetches the HTML of the Gargoyle page and parses the information into the profiles, appending new profiles as needed
-    void update(QString url, QList<GargoyleProfile> profiles);
+    bool update(QString url, QList<GargoyleProfile> profiles);
 };
 
 #endif // GARGOYLEPARSER_H

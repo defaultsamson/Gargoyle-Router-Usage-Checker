c164316037d71a6f6f6c9b786d682485d7e29122
Samson Close <stanmoon95@gmail.com>
2020-08-27 20:27:18 -0400

    Added HTML fetcher to Gargoyle Parser

--- gargoyleparser.cpp
+++ gargoyleparser.cpp
@@ -1,5 +1,11 @@
 #include "gargoyleparser.h"
 
+#include <QUrl>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QEventLoop>
+
 GargoyleParser::GargoyleParser()
 {
 
@@ -7,4 +13,12 @@ GargoyleParser::GargoyleParser()
 
 void GargoyleParser::update(QString url, QList<GargoyleProfile> profiles)
 {
+    QUrl url = QUrl("http://192.168.1.1");
+    QNetworkAccessManager manager;
+    QNetworkRequest request(url);
+    QNetworkReply *reply(manager.get(request));
+    QEventLoop loop;
+    QObject::connect(reply, SIGNAL(finished()), &loop, SLOT(quit()));
+    loop.exec();
+    qDebug(reply->readAll());
 }
